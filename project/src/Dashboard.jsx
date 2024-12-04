07d95a8f6bbf7ce432ca3dbc5c1d37f6b3896f23
tikuzaa <dey.debangan2004@gmail.com>
2024-12-05 01:48:55 +0530

    front end updated

--- project/src/Dashboard.jsx
+++ project/src/Dashboard.jsx
@@ -13,35 +13,33 @@ import {
   getIssues4,
   getPullRequests5,
   getIssues5,
-  getPullRequests6,
-  getIssues6,
 } from "./callback/callback";
 import UserDashboard from "./UserInformation";
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const [user, setUser] = useState(null);
-  //sayan
+  //debangan
   const [pullRequests, setPullRequests] = useState([]);
   const [issues, setIssues] = useState([]);
   //bidisha
   const [pullRequests1, setPullRequests1] = useState([]);
   const [issues1, setIssues1] = useState([]);
-  //ankit
+  //muskaan
   const [pullRequests2, setPullRequests2] = useState([]);
   const [issues2, setIssues2] = useState([]);
-  //debangan
+  //sayandip
   const [pullRequests3, setPullRequests3] = useState([]);
   const [issues3, setIssues3] = useState([]);
-  //sayandip
+  //sayan
   const [pullRequests4, setPullRequests4] = useState([]);
   const [issues4, setIssues4] = useState([]);
   //sahil
   const [pullRequests5, setPullRequests5] = useState([]);
   const [issues5, setIssues5] = useState([]);
-  //muskaan
-  const [pullRequests6, setPullRequests6] = useState([]);
-  const [issues6, setIssues6] = useState([]);
+  // //ankit
+  // const [pullRequests6, setPullRequests6] = useState([]);
+  // const [issues6, setIssues6] = useState([]);
 
   const navigate = useNavigate();
 
@@ -49,27 +47,27 @@ function Dashboard() {
     localStorage.removeItem("access_token");
     setUser(null);
     navigate("/");
-    //sayan
+    //debangan
     setPullRequests([]);
     setIssues([]);
     //bidisha
     setPullRequests1([]);
     setIssues1([]);
-    //ankit
+    //muskaan
     setPullRequests2([]);
     setIssues2([]);
-    //debangan
+    //sayandip
     setPullRequests3([]);
     setIssues3([]);
-    //sayandip
+    //sayan
     setPullRequests4([]);
     setIssues4([]);
     //sahil
     setPullRequests5([]);
     setIssues5([]);
-    //Muskaan
-    setPullRequests6([]);
-    setIssues6([]);
+    // //Muskaan
+    // setPullRequests6([]);
+    // setIssues6([]);
   };
 
   useEffect(() => {
@@ -82,40 +80,40 @@ function Dashboard() {
 
         // Fetch pull requests after user data is retrieved
         //debangan
-        const prData3 = await getPullRequests3(token);
-        setPullRequests3(prData3);
-        const issueData3 = await getIssues3(token);
-        setIssues3(issueData3);
+        const prData = await getPullRequests(token);
+        setPullRequests(prData);
+        const issueData = await getIssues(token);
+        setIssues(issueData);
         //bidisha
         const prData1 = await getPullRequests1(token);
         setPullRequests1(prData1);
         const issueData1 = await getIssues1(token);
         setIssues1(issueData1);
-        //Muskaan
-        const prData6 = await getPullRequests6(token);
-        setPullRequests6(prData6);
-        const issueData6 = await getIssues6(token);
-        setIssues6(issueData6);
+        // muskaan
+        const prData2 = await getPullRequests2(token);
+        setPullRequests2(prData2);
+        const issueData2 = await getIssues2(token);
+        setIssues2(issueData2);
         //sayandip
+        const prData3 = await getPullRequests3(token);
+        setPullRequests3(prData3);
+        const issueData3 = await getIssues3(token);
+        setIssues3(issueData3);
+        //sayan
         const prData4 = await getPullRequests4(token);
         setPullRequests4(prData4);
         const issueData4 = await getIssues4(token);
         setIssues4(issueData4);
-        //sayan
-        const prData = await getPullRequests(token);
-        setPullRequests(prData);
-        const issueData = await getIssues(token);
-        setIssues(issueData);
         //sahil
         const prData5 = await getPullRequests5(token);
         setPullRequests5(prData5);
         const issueData5 = await getIssues5(token);
         setIssues5(issueData5);
-        //ankit
-        const prData2 = await getPullRequests2(token);
-        setPullRequests2(prData2);
-        const issueData2 = await getIssues2(token);
-        setIssues2(issueData2);
+        // //ankit
+        // const prData6 = await getPullRequests6(token);
+        // setPullRequests6(prData6);
+        // const issueData6 = await getIssues6(token);
+        // setIssues6(issueData6);
       }
 
       fetchUserData();
@@ -140,8 +138,6 @@ function Dashboard() {
           issues4={issues4}
           pullRequests5={pullRequests5}
           issues5={issues5}
-          pullRequests6={pullRequests6}
-          issues6={issues6}
           handleLogout={handleLogout}
         />
       ) : null}
--- project/src/components/About.jsx
+++ project/src/components/About.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Reveal } from "./Reveal";
+import { FaDiscord } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="min-h-screen bg-gray-200 flex items-center justify-center px-4 sm:px-6 lg:px-8">
-      <div className="flex flex-col min-[1126px]:flex-row max-w-[69rem] w-full">
-        <div className="flex-col min-[711px]:text-left m-4 pt-[6.5rem] max-[711px]:text-center items-center justify-center">
-          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-oxanium font-bold text-[#004825] mb-6 max-[711px]:w-full">
+    <div className="min-h-screen overflow-hidden bg-gray-200 flex items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className="flex flex-col  min-[1126px]:flex-row max-w-[69rem] w-full">
+        <div className="flex-col relative min-[711px]:text-left m-4 pt-[6.5rem] max-[711px]:text-center items-center justify-center">
+          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-oxanium font-bold text-[#003c54] mb-6 relative bottom-[1.5rem] w-[40rem] max-[711px]:w-full">
             <Reveal>About</Reveal>
-          </h2>
-          <h2 className="text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-oxanium font-bold text-[#004825] mb-6 relative bottom-[1.5rem] w-[40rem] max-[711px]:w-full">
             <Reveal>Open Source</Reveal>
           </h2>
           <Reveal>
@@ -29,20 +28,20 @@ const About = () => {
             </p>
             <ul className="text-black text-base sm:text-lg mb-4 w-full font-oxanium text-left">
               <li>
-                <span className="text-[#004825] font-semibold">
+                <span className="text-[#003c54] font-semibold">
                   Collaborate:
                 </span>{" "}
                 Join a passionate community and work together on meaningful
                 projects.
               </li>
               <li>
-                <span className="text-[#008147] font-semibold">
+                <span className="text-[#007987] font-semibold">
                   Enhance Your Skills:
                 </span>{" "}
                 Gain real-world experience and learn new technologies.
               </li>
               <li>
-                <span className="text-[#00a05a] font-semibold">
+                <span className="text-[#0098a9] font-semibold">
                   Build Your Portfolio:
                 </span>{" "}
                 Showcase your contributions to stand out in the job market.
@@ -50,10 +49,8 @@ const About = () => {
             </ul>
           </Reveal>
         </div>
-
-        <div className="mx-5"></div>
         <Reveal>
-          <div className="flex-col justify-between bg-[#5dffa2] h-full p-5 mt-10 min-[1126px]:mt-40 min-[1126px]:ml-20 rounded-lg flex-grow mb-[6.5rem]">
+          <div className="flex-col justify-between bg-[#aee9ff] h-full p-5 mt-10 min-[1126px]:mt-40 min-[1126px]:ml-20 rounded-lg flex-grow mb-[6.5rem]">
             {/* Text Section */}
             <div className="text-center text-black mt-5 mb-8">
               <h2 className="text-xl sm:text-2xl font-oxanium font-bold">
@@ -67,9 +64,16 @@ const About = () => {
             </div>
             {/* Button Section */}
             <div className="flex justify-center items-center m-5">
-              <a href="https://discord.gg/DU9HBZdk3m" target="blank">
-                <button className="bg-[#008217] text-white w-full sm:w-48 h-12 font-oxanium font-semibold py-3 px-4 rounded-full hover:bg-[#00a05a] hover:text-black transition duration-300">
-                  Join Discord
+              <a
+                href="https://discord.gg/DU9HBZdk3m"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="bg-[#003c54] relative group w-full sm:w-48 h-12 font-oxanium font-semibold py-3 px-4 rounded-full flex items-center justify-center transition duration-300 hover:bg-[#00749d] hover:text-black">
+                  <span className="transition-transform duration-300 group-hover:translate-x-[-10px] text-white opacity-100">
+                    Join Discord
+                  </span>
+                  <FaDiscord className="opacity-0 text-white transition-opacity duration-300 group-hover:opacity-100" />
                 </button>
               </a>
             </div>
